38500ceefc8fe93ce39e0ce7c0eb59e9899f4779
Jonatan H Sundqvist <jonatanhsundqvist@gmail.com>
2015-02-09 03:20:11 +0100

    1) Added TODOs 2) Added MTLToken type and MTL type synonym 3) Tweaked and added section headers 4) Created global functions for some parsing subexpressions 5) Added parser (if that is the correct term) for MTL data

--- Wavefront.hs
+++ Wavefront.hs
@@ -12,9 +12,15 @@
 --        - Improve naming scheme
 --        - Separate MTL and OBJ parsers (?)
 --        - Additional attributes (lighting, splines, etc.)
+--        - FFI
+--        - Debugging information (line number, missing file, missing values, etc.)
 
 -- SPEC | -
 --        -
+
+
+
+module Wavefront (OBJToken, OBJ, parseOBJ) where
 
 
 
@@ -50,26 +56,38 @@ data OBJToken = Vertex  Float Float Float |
 type OBJ = [Maybe OBJToken]
 
 -- | 
--- data MTLToken = 
+data MTLToken = Ambient  Float Float Float Float | -- Ka
+                Diffuse  Float Float Float Float | -- Kd
+                Specular Float Float Float Float | -- Ks
+
+				MapDiffuse String | -- map_Kd
+		        Material   String   -- newmtl
+		-- ('Ns', 'Ni', 'd', 'Tr', 'illum')
 
 -- |
--- type MTL = [Maybe MTLToken]
+type MTL = [Maybe MTLToken]
 
 
 
 ---------------------------------------------------------------------------------------------------
--- Functions
+-- Functions (pure)
 ---------------------------------------------------------------------------------------------------
+-- Parsers ----------------------------------------------------------------------------------------
 -- |
 -- TODO: Use appropriate container type (cf. TODO section)
 -- TODO: Extract filter predicate (isComment, isEmpty)
 -- TODO: Is it even necessary to strip whitespace?
 -- TODO: Function for composing predicates (?)
+-- TODO: Should this function separate the various fields (eg. [(Vertices, Faces, Materials, Groups)] instead of [Maybe OBJToken])
 parseOBJ :: String -> OBJ
-parseOBJ = map parseOBJRow . filter (\ ln -> not $ isPrefixOf "#" (dropWhile isSpace ln) || null ln) . lines
+parseOBJ = map parseOBJRow . rows
+
+-- I never new pattern matching in list comprehensions could be used to filter by constructor
+-- let rows = parseOBJ data in ([ v | @v(Vertex {}) <- rows], [ v | @v(Vertex {}) <- rows])
 
 
 -- |
+-- TODO: Correctness (complete function, no runtime exceptions)
 -- TODO: Rename 'which' (?)
 -- TODO: Handle invalid rows
 -- TODO: Extract value parsing logic (eg. pattern matching, converting, handle errors)
@@ -85,24 +103,64 @@ parseOBJRow ln = let (which:values) = words ln in case which of
   "vt" -> Just $ vector Texture values -- Texture
   -- TODO: Clean this up
   -- TODO: Handle invalid data
-  "f"  -> Just . Face   $ map (vector triplet . filter (/="/") . groupBy ((==) `on` (=='/'))) values -- Face
+  "f"  -> Just . Face   $ map (vector triplet . splitOn '/') values -- Face
   "g"  -> Just . Group  $ head values -- Group
   "o"  -> Just . Object $ head values -- Object
   "s"  -> Nothing -- Smooth shading
   "mtllib" -> Just . LibMTL $ head values
   "usemtl" -> Just . UseMTL $ head values
   _ -> Nothing
-  where vector token (x:y:z:[]) = token (read x) (read y) (read z) -- TODO: Add back the Maybe wrapper (?)
-        -- vector _      _         = Nothing
-        triplet a b c = (a, b, c) -- TODO: Use tuple sections
+  where triplet a b c = (a, b, c) -- TODO: Use tuple sections
 
 
 -- |
 -- process the OBJ tokens
+parseMTL :: String -> MTL
+parseMTL = map parseMTLRow . rows
+
+
+-- | 
+-- process the MTL tokens
+-- TODO: cf. parseOBJRow
+parseMTLRow :: String -> Maybe MTLToken
+parseMTLRow ln = let (which:values) = words ln in case which of
+  "Ka" -> Just $ withChannels Ambient  values -- Ka
+  "Kd" -> Just $ withChannels Diffuse  values -- Kd
+  "Ks" -> Just $ withChannels Specular values -- Ks
+  "map_Kd" -> let (name:[]) = values in Just $ MapDiffuse name -- map_Kd
+  "newmtl" -> let (name:[]) = values in Just $ Material   name -- newmtl
+  _        -> Nothing
+  where withChannels token rgba = let (r:g:b:a:[]) = rgba in token (read r) (read g) (read b) (read a) 
+
+
+-- Parsing utilities ------------------------------------------------------------------------------
+-- |
+splitOn :: Eq a => a -> [a] -> [[a]]
+splitOn c s = filter (/=[c]) . groupBy ((==) `on` (==c)) $ s
 
 
 -- |
--- process the MTL tokens
+-- TODO: Drop comments at the end of a line (?)
+isComment :: String -> Bool
+isComment = isPrefixOf "#" . dropWhile isSpace
+
+
+-- |
+rows :: String -> [String]
+rows = filter (\ ln -> not $ any ($ ln) [null, isComment]) . lines
+
+
+-- |
+vector :: Read r => (r -> r -> r -> b) -> [String] -> b
+vector token (x:y:z:[]) = token (read x) (read y) (read z) -- TODO: Add back the Maybe wrapper (?)
+-- vector _      _         = Nothing
+
+
+
+---------------------------------------------------------------------------------------------------
+-- Functions (IO)
+---------------------------------------------------------------------------------------------------
+
 
 
 
